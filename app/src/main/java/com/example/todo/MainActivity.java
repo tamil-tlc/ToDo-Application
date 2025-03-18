a206c5d198e06933d79acaf2b2f95c524f45c5e9
Tamil2031 <tamil21112001>
2025-03-18 21:57:59 +0530

    add hello world

---
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.todo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
